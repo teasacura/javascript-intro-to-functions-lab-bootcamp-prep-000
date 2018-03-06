570f332d2522cafea5cc2adea59e516aa23efae4
Eva Leake <teasacuraster@gmail.com>
2018-03-06 00:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
-  {return "I can't hear you!"
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!"
 } else {
   return "YES INDEED!"}
 }
